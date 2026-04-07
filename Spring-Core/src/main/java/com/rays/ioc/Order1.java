c4071d8cc3eff6164acc02d477679991e712d883
Neeraj4141 <mewadaneeraj368@gmail.com>
2026-04-07 15:17:56 +0530

    done

---
+++ Spring-Core/src/main/java/com/rays/ioc/Order1.java
@@ -0,0 +1,40 @@
+package com.rays.ioc;
+
+public class Order1 {
+
+	private Inventory inventory;
+	private Payment payment;
+
+	public Inventory getInventory() {
+		return inventory;
+	}
+
+	public void setInventory(Inventory inventory) {
+		this.inventory = inventory;
+	}
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
+	public void bookticket(int items) {
+
+		int price = 10;
+
+		double totalAmount = items * price;
+		double updatedBalance = payment.makepayment(totalAmount);
+		int updatedStock = inventory.sold(items);
+
+		System.out.println("Tickets are Booked");
+		System.out.println("Total Amount Paid: " + totalAmount);
+		System.out.println("Remaining Balance: " + updatedBalance);
+		System.out.println("Total Booked Tickects: " + items);
+		System.out.println("Updated Stock: " + updatedStock);
+
+	}
+
+}
---
+++ Spring-Core/src/main/java/com/rays/ioc/TestOrder1.java
@@ -0,0 +1,17 @@
+package com.rays.ioc;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+public class TestOrder1 {
+	public static void main(String[] args) {
+
+		BeanFactory factory = new XmlBeanFactory(new ClassPathResource("Order1.xml"));
+
+		Order1 order1 = (Order1) factory.getBean("order1");
+
+		order1.bookticket(5);
+	}
+
+}
